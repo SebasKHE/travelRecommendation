184043a38ab988e1857d73b1068cd87343f3c2d0
SebasKHE <sebasttcaicedo@gmail.com>
2025-02-09 10:52:29 -0500

    mmodifications

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,6 +1,14 @@
-fetch('./travel_recommendation_api.json')
+fetch('travel_recommendation_api.json')
 .then(response => response.json())
 .then(data => {
-  console.log(data);
+  let citesData = data.name;
   // Do something with your data
 })
+
+
+.catch(error => {
+    console.error('Error:', error);
+    divContent.innerHTML = '<p class="error">Error loading cities data</p>';
+})
+
+divContent.innerHTML = `<p>"${data} </p>`
